f3dc5912791c1051a855fccdf16304abc6d4be94
taxicrabb <152030282+taxicrabb@users.noreply.github.com>
2023-12-04 18:50:50 -0800

    Add files via upload

---
+++ diceaddiction.js
@@ -0,0 +1,547 @@
+Hooks.on('diceSoNiceReady', (dice3d) => {
+	async function loadTextures(){
+		await dice3d.addTexture("Aluminium", {
+			name: "📱 Aluminium",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/Aluminium.jpg",
+			bump: "modules/diceaddiction/textures/bump/Aluminium.jpg"
+		});
+		await dice3d.addTexture("Sand", {
+			name: "📱 Sand",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/sand.jpg",
+			bump: "modules/diceaddiction/textures/sand.jpg"
+		});
+		await dice3d.addTexture("BurningHell", {
+			name: "📱 BurningHell",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/BurningHell.jpg",
+			bump: "modules/diceaddiction/textures/BurningHell.jpg"
+		});
+		await dice3d.addTexture("Sparks", {
+			name: "📱 Sparks",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/sparks.jpg",
+		});
+		await dice3d.addTexture("FluffyClouds", {
+			name: "📱 Fluffy Clouds",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/fluffyclouds.jpg",
+		});
+		await dice3d.addTexture("BigSparks", {
+			name: "📱 Big Sparks",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/bigsparks.jpg",
+			bump: "modules/diceaddiction/textures/bigsparks.jpg"
+		});
+		await dice3d.addTexture("FabulousFire", {
+			name: "📱 Fabulous Fire",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/fabulousfire.jpg",
+			bump: "modules/diceaddiction/textures/fabulousfire.jpg"
+		});
+		await dice3d.addTexture("Boiling", {
+			name: "📱 Boiling",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/boiling.jpg",
+			bump: "modules/diceaddiction/textures/boiling.jpg"
+		});
+		await dice3d.addTexture("Fabulous", {
+			name: "📱 Fabulous",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/fabulous.jpg",
+		});
+		await dice3d.addTexture("Magma", {
+			name: "📱 Magma",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/magma.jpg",
+			bump: "modules/diceaddiction/textures/magma.jpg"
+		});
+		await dice3d.addTexture("Tab", {
+			name: "📱 Tab",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/tab.jpg",
+			bump: "modules/diceaddiction/textures/tab.jpg",
+		});
+		await dice3d.addTexture("FrozenLake", {
+			name: "📱 Frozen Lake",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/frozenlake.jpg",
+			bump: "modules/diceaddiction/textures/bump/frozenlake.jpg",
+		});
+		await dice3d.addTexture("Valley", {
+			name: "📱 Valley",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/valley.jpg",
+			bump: "modules/diceaddiction/textures/valley.jpg"
+		});	
+		await dice3d.addTexture("GreenRavine", {
+			name: "📱 Green Ravine",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/valley2.jpg"
+		});	
+		await dice3d.addTexture("shadesofmountain", {
+			name: "📱 ShadesOfMountain",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/shadesofmountain.jpg",
+			bump: "modules/diceaddiction/textures/shadesofmountain.jpg"
+		});
+		await dice3d.addTexture("Swirl", {
+			name: "📱 Swirl",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/swirl.jpg",
+		});
+		await dice3d.addTexture("Blatt", {
+			name: "📱 Blatt",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/blatt.jpg",
+			bump: "modules/diceaddiction/textures/blatt.jpg"
+		});
+		await dice3d.addTexture("Vortex", {
+			name: "📱 Vortex",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/vortex.png",
+			bump: "modules/diceaddiction/textures/vortex.png",
+		});
+		await dice3d.addTexture("Dark_Cloudy", {
+			name: "📱 Dark_Cloudy",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/dark_cloudy.jpg",
+			bump: "modules/diceaddiction/textures/dark_cloudy.jpg",
+		});	
+		await dice3d.addTexture("Mood Dice", {
+			name: "📱 Mood Dice",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/mood.jpg",
+			bump: "modules/diceaddiction/textures/mood.jpg"
+		});
+		await dice3d.addTexture("9oclock", {
+			name: "📱 9 O'Clock",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/9oclock.jpg",
+			bump: "modules/diceaddiction/textures/9oclock.jpg",
+		});	
+		await dice3d.addTexture("Redgrad", {
+			name: "📱 Redgrad",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/redgrad.jpg",
+			bump: "modules/diceaddiction/textures/redgrad.jpg",
+		});
+		await dice3d.addTexture("Bloody", {
+			name: "📱 Bloody",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/bloody.jpg",
+			bump: "modules/diceaddiction/textures/bloody.jpg"
+		});
+		await dice3d.addTexture("Galaxy", {
+			name: "📱 Galaxy",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/galaxy.jpg"
+		});
+		await dice3d.addTexture("Astral", {
+			name: "📱 Astral",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/astral.jpg"
+		});
+		await dice3d.addTexture("Mahagoni", {
+			name: "📱 Mahagoni",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/mahagoni.jpg",
+			bump: "modules/diceaddiction/textures/mahagoni.jpg"
+		});
+		await dice3d.addTexture("CarvedWood", {
+			name: "📱 Carved Wood",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/carved.jpg",
+			bump: "modules/diceaddiction/textures/carved.jpg",
+		});
+		await dice3d.addTexture("MagicalForest", {
+			name: "📱 Magical Forest",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/magicalforest.jpg",
+			bump: "modules/diceaddiction/textures/magicalforest.jpg",
+		}); 
+		await dice3d.addTexture("Waves", {
+			name: "📱 Waves",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/waves.jpg",
+			bump: "modules/diceaddiction/textures/waves.jpg",
+		});
+		await dice3d.addTexture("Duality", {
+			name: "📱 Duality",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/duality.jpg",
+		});
+ 		await dice3d.addTexture("Duality2", {
+			name: "📱 Duality2",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/duality2.jpg",
+		});
+  		await dice3d.addTexture("Duality3", {
+			name: "📱 Duality3",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/duality3.jpg",
+		}); 
+		await dice3d.addTexture("Cobble", {
+			name: "📱 Cobble",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/cobble.jpg",
+			bump: "modules/diceaddiction/textures/cobble.jpg",
+		});
+		await dice3d.addTexture("Cobble_Dark", {
+			name: "📱 Cobble_Dark",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/darkcobble.jpg",
+			bump: "modules/diceaddiction/textures/darkcobble.jpg",
+		});
+		await dice3d.addTexture("DarkCrystal", {
+			name: "📱 DarkCrystal",
+			composite: "multiply",
+			source: "modules/diceaddiction/textures/darkcrystal.jpg",
+			bump: "modules/diceaddiction/textures/bump/crystal.jpg"
+		});
+		loadColors();
+	}
+	async function loadColors(){
+		dice3d.addColorset({
+			name: 'gjade',
+			description: "📱 Golden-Jade",
+			category: "diceaddiction",
+			foreground: '#9F8003',
+			background: "#039f32",
+			texture: 'Duality',
+			edge: '#9F8003',
+			material: 'metal',
+			font: '📱 Jade',
+			fontScale:{
+			   "d6":1.1,
+			   "df":2.5
+			},
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'DarkWood',
+			description: "📱 Dark Wood",
+			category: "diceaddiction",
+			texture: 'Mahagoni',
+			foreground: '#382600',
+			background: "#2d1301",
+			outline: '#563201',
+			edge: '#1f1200',
+			material: 'wood',
+			font: '📱 Lumber',
+			fontScale:{
+			   "d6":1.1,
+			   "df":2.5
+			},
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'Burn',
+			description: "📱 Burn",
+			category: "diceaddiction",
+			texture: 'BurningHell',
+			foreground: '#ffffff',
+			background: "#ff4000",
+			outline: '#ff8800',
+			edge: '#cc7700',
+			material: 'metal',
+			font: '📱 Fire',
+			fontScale:{
+			   "d6":1.1,
+			   "df":2.5
+			},
+			visibility: 'visible'
+		},"default");
+
+		dice3d.addColorset({
+			name: 'GreenGalaxy',
+			description: "📱 GreenGalaxy",
+			category: "diceaddiction",
+			texture: 'Galaxy',
+			foreground: '#404040',
+			background: "#2cba5d",
+			outline: '#ffffff',
+			edge: '#094d00',
+			material: 'metal',
+			font: 'American Typewriter',
+			fontScale:{
+			   "d6":1.1,
+			   "df":2.5
+			},
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'FrostyCrystal',
+			description: "📱 FrostyCrystal",
+			category: "diceaddiction",
+			texture: 'DarkCrystal',
+			foreground: '#FFFFFF',
+			background: "#0150b7",
+			outline: '#28cacc',
+			edge: '#28b4cc',
+			material: 'metal',
+			font: 'American Typewriter',
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'Duality',
+			description: "📱 Duality (Red/Blue)",
+			category: "diceaddiction",
+			texture: 'Duality',
+			foreground: '#ffffff',
+			background: "#ffffff",
+			outline: '#ffffff',
+			edge: '#ffffff',
+			material: 'metal',
+			font: '📱 Lumber',
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'Duality-2',
+			description: "📱 Duality (Red/Green)",
+			category: "diceaddiction",
+			texture: 'Duality2',
+			foreground: '#00ff2a',
+			background: "#ceff47",
+			outline: '#ffffff',
+			edge: '#2bff00',
+			material: 'metal',
+			font: '📱 Lumber',
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'Duality-3',
+			description: "📱 Duality (Green)",
+			category: "diceaddiction",
+			texture: 'Duality3',
+			foreground: '#ffffff',
+			background: "#61ff9e",
+			outline: '#61ff9e',
+			edge: '#61ff9e',
+			material: 'metal',
+			font: '📱 Lumber',
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'GreenCloud',
+			description: "📱 GreenCloud",
+			category: "diceaddiction",
+			texture: 'Dark_Cloudy',
+			foreground: '#00ff33',
+			background: "#00ff33",
+			outline: '#e1ff00',
+			edge: '#e1ff00',
+			material: 'metal',
+			font: 'Arial',
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'YellowRavine',
+			description: "📱 YellowRavine",
+			category: "diceaddiction",
+			texture: 'GreenRavine',
+			foreground: '#665500',
+			background: "#141414",
+			outline: '#ffffff',
+			edge: '#ffee00',
+			material: 'metal',
+			font: 'Arial',
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'Bloodlines',
+			description: "📱 Bloodlines",
+			category: "diceaddiction",
+			texture: 'BigSparks',
+			foreground: '#660000',
+			background: "#3d0000",
+			outline: '#ffffff',
+			edge: '#ff0000',
+			material: 'metal',
+			font: '📱 Immortal',
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'Necromancer',
+			description: "📱 Necromancer",
+			category: "diceaddiction",
+			texture: 'DarkCrystal',
+			foreground: '#080808',
+			background: "#310707",
+			outline: '#ff0000',
+			edge: '#000000',
+			material: 'metal',
+			font: '📱 Horror',
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'Fabulous Fire',
+			description: "📱 Fabulous Fire",
+			category: "diceaddiction",
+			texture: 'FabulousFire',
+			foreground: '#ffae00',
+			background: "#ffd500",
+			outline: '#ff9500',
+			edge: '#ffffff',
+			material: 'metal',
+			font: '📱 Forest2',
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'Fabulous Fire',
+			description: "📱 Fabulous Fire",
+			category: "diceaddiction",
+			texture: 'FabulousFire',
+			foreground: '#ffae00',
+			background: "#ffd500",
+			outline: '#ff9500',
+			edge: '#ffffff',
+			material: 'metal',
+			font: '📱 Forest2',
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'Fabulous Fire',
+			description: "📱 Fabulous Fire",
+			category: "diceaddiction",
+			texture: 'FabulousFire',
+			foreground: '#ffae00',
+			background: "#ffd500",
+			outline: '#ff9500',
+			edge: '#ffffff',
+			material: 'metal',
+			font: '📱 Forest2',
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'FluffyCloud',
+			description: "📱 Fluffy Cloud",
+			category: "diceaddiction",
+			texture: 'FluffyClouds',
+			foreground: '#002738',
+			background: "#ffffff",
+			outline: '#ffffff',
+			edge: '#ffffff',
+			material: 'metal',
+			font: '📱 Air',
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'Pirate',
+			description: "📱 Pirate",
+			category: "diceaddiction",
+			texture: 'CarvedWood',
+			foreground: '#FFFFFF',
+			background: "#757575",
+			outline: '#6a3401',
+			edge: '#6a3401',
+			material: 'wood',
+			font: '📱 Pirates',
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'Frozen Lake',
+			description: "📱 Frozen Lake",
+			category: "diceaddiction",
+			texture: 'FrozenLake',
+			foreground: '#FFFFFF',
+			background: "#3b56ba",
+			outline: '#70fffd',
+			edge: '#70fffd',
+			material: 'glass',
+			font: '📱 Iceberg',
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'Forest',
+			description: "📱 Forest",
+			category: "diceaddiction",
+			texture: 'CarvedWood',
+			foreground: '#FFFFFF',
+			background: "#6ed548",
+			outline: '#04ff00',
+			material: 'metal',
+			font: '📱 Forest2',
+			visibility: 'visible'
+		},"default");
+		dice3d.addColorset({
+			name: 'ShadesOfMountain',
+			description: "📱 Mountain",
+			category: "diceaddiction",
+			texture: 'shadesofmountain',
+			foreground: '#ff7b00',
+			background: "#ffffff",
+			outline: '#470000',
+			edge: '#3d0000',
+			material: 'glass',
+			visibility: 'visible'
+		},"default");
+	}
+	loadTextures();
+	loadFaces();
+	async function loadFaces(){
+		await dice3d.addSystem({id:"diceaddiction",name:"📱 GoldenAnvil20 (d20, 20 best)"},false);
+		await dice3d.addSystem({id:"diceaddictiona",name:"📱 GoldenAnvil1 (d20, 1 best)"},false);
+		await dice3d.addSystem({id:"diceaddictionanswer",name:"📱 TheAnswer (d20, 20 best)"},false);
+		await dice3d.addSystem({id:"diceaddictionanswer2",name:"📱 TheAnswer (d20, 1 best)"},false);
+		// FOUNDRY DICE
+		await dice3d.addDicePreset({
+			type:"d20",
+			labels:[
+			  '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19',
+			  'modules/diceaddiction/faces/foundrynat20.png'
+			],
+			bumpMaps: [,,,,,,,,,,,,,,,,,,,
+				"modules/diceaddiction/faces/foundrynat20_bump.webp"
+			],
+
+			system:"diceaddiction"
+		});	
+		await dice3d.addDicePreset({
+			type:"d20",
+			labels:[
+			  'modules/diceaddiction/faces/foundrynat20.png','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19',
+			  '20'
+			],
+			bumpMaps: ["modules/diceaddiction/faces/foundrynat20_bump.webp",,,,,,,,,,,,,,,,,,,
+			],
+			system:"diceaddictiona"
+		});
+		//ANSWER TO ALL
+		await dice3d.addDicePreset({
+			type:"d20",
+			labels:[
+			  '1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19',
+			  'modules/diceaddiction/faces/theanswertoall.png'
+			],
+			bumpMaps: [,,,,,,,,,,,,,,,,,,,
+				"modules/diceaddiction/faces/theanswertoall_bump.png"
+			],
+
+			system:"diceaddictionanswer"
+		});	
+		await dice3d.addDicePreset({
+			type:"d20",
+			labels:[
+			  'modules/diceaddiction/faces/theanswertoall.png','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19',
+			  '20'
+			],
+			bumpMaps: ["modules/diceaddiction/faces/theanswertoall_bump.png",,,,,,,,,,,,,,,,,,,
+			],
+			system:"diceaddictionanswer2"
+		});
+		
+	}
+	
+	async function loadFontDice(){
+		dice3d.addColorset({
+			name: 'FontDie',
+			description: "📱 FontDie",
+			category: "diceaddiction",
+			font: ['📱 KaushanScript'],
+			visibility: 'hidden'
+		},"default");
+	}
+	
+});
